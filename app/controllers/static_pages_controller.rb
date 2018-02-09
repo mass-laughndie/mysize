add5b3afd84a0a30b79a46889b822f4e6bb7e8d4
Masaya Takahashi <masaya.oor710@gmail.com>
2018-02-09 19:08:56 +0900

    Finish create static pages

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,19 @@
+class StaticPagesController < ApplicationController
+  def home
+  end
+
+  def help
+  end
+
+  def about
+  end
+
+  def terms
+  end
+
+  def privacy
+  end
+
+  def contact
+  end
+end
