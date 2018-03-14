189c4dad90e76b41732a82893a996565261b0703
Masaya Takahashi <masaya.oor710@gmail.com>
2018-03-15 01:05:49 +0900

    Setting AWS

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -23,12 +23,16 @@ end
 CarrierWave.configure do |config|
   #テスト時の画像の格納場所を変更 => gitに保存されなくなる
   config.cache_dir = Rails.root.join 'tmp/uploads'
-  
-=begin
-  if Rails.env.development? || Rails.env.production?
-    config.storage :file
-  elsif Rails.env.test?
-    config.strage NullStorage
+
+  if Rails.env.production?
+
+    config.fog_credentials = {
+      provider:               'AWS',
+      region:                 ENV['S3_REGION'],
+      aws_access_key_id:      ENV['S3_ACCESS_KEY'],
+      aws_secret_access_key:  ENV['S3_SECRET_KEY']
+    }
+    config.fog_directory = ENV['S3_BUCKET']
+
   end
-=end
 end
