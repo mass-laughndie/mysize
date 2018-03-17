a1828a2b337dc6a1db18cf0cf0d680aa8de1bc92
Masaya Takahashi <masaya.oor710@gmail.com>
2018-03-17 19:59:54 +0900

    Add js tag link & Modify js id link

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -113,26 +113,32 @@ function indexId(){
   })
 }
 
-//comment返信ユーザーのリンク化
+
+//@IDのリンク化
 function changeLink(_iid) {
   $('.autolink').each(function(){
     var
       txt = $(this).html(),
-      exp = txt.match(/@[a-zA-Z0-9_]+\s/g);                //全「@ID 」
+      exp = txt.match(/@[a-zA-Z0-9_]+?(\s|<br>|<\/span>)/g);    //全「@ID(空白|<br>|</span>)」
+    exp = Array.from(new Set(exp));                        //重複削除
     if(exp != null){
       for(var i = 0; i < exp.length; i++){
         var
-          elength = exp[i].length,                         //文字数
-          msid = exp[i].substring(1, elength - 1);         //「ID」
+          elength = exp[i].length,                            //文字数
+          msid = exp[i].replace(/@|\s|<br>|<\/span>/g, '');   //「ID」
         //indexid内のものと一致する場合リンク化
-        if (iid.indexOf(msid) >= 0){
+        if (iid.indexOf(msid) >= 0) {
           var
             url = window.location.protocol + "//"
                   + window.location.host + '/'
-                  + msid + "?display=square",
-            str = exp[i].substring(0, elength - 1),         //「@ID」
-            txt = $(this).html();             //新たに定義しないと複数置換できない
-          $(this).html(txt.replace(str, "<a class='com-link' href=" + url + ">" + str + "</a>"));
+                  + msid + "?display=square",                 //リンクURL
+            str = exp[i].replace(/\s|<br>|<\/span>/g, ''),    //「@ID」
+            option = exp[i].replace(str, ''),                 //msidの後続
+            txt = $(this).html(),             //新たに定義しないと複数置換できない
+            //new RegExp(exp[i], 'g')で重複を一括replace
+            //exp[i]は重複(=完全一致)以外は一意の文字列のため、exp[i]でreplaceして削られるoptionを後から追加
+            replaceText = txt.replace(new RegExp(exp[i], 'g'), "<a class='id-link' href=" + url + ">" + str + "</a>" + option);
+          $(this).html(replaceText);
         }
       }
     }
@@ -277,6 +283,7 @@ document.addEventListener('turbolinks:load', function() {
   });
 });
 
+/* (autolinkとの相性×)
 //searchの該当文字を太文字に変更(要com-link内検索対応)
 document.addEventListener('turbolinks:load', function() {
   $(function() {
@@ -299,5 +306,35 @@ document.addEventListener('turbolinks:load', function() {
         });
       }
     }
+  });
+});
+*/
+
+//ハッシュタグのリンク化
+document.addEventListener('turbolinks:load', function() {
+  $(function() {
+    $('.autolink').each(function(){
+      var
+        txt = $(this).html(),
+        exp = txt.match(/#\S+?(\s|<br>)/g),     //全「#(任意の文字列)(空白or<br>」
+        exp = Array.from(new Set(exp));         //重複削除
+    if ( exp != null ){
+      for ( var i = 0; i < exp.length; i++ ) {
+        var
+          elength = exp[i].length,                          //文字数
+          word = exp[i].replace(/\s|<br>|<\/span>/g, ''),   //「#(任意の文字列)」
+          option = exp[i].replace(word, ''),                //wordの後続
+          key = "%23" + word.slice(1)                       //「#」削除
+          url = window.location.protocol + "//"
+                + window.location.host
+                + '/search?for=post&keyword=' + key,        //リンクURL
+          txt = $(this).html(),             //新たに定義しないと複数置換できない
+          //new RegExp(exp[i], 'g')で重複を一括replace
+          //exp[i]は重複(=完全一致)以外は一意の文字列のため、exp[i]でreplaceして削られるoptionを後から追加
+          replaceText = txt.replace(new RegExp(exp[i], 'g'), "<a class='tag-link' href=" + url + ">" + word + "</a>" + option);
+        $(this).html(replaceText);
+        }
+      }
+    });
   });
 });
