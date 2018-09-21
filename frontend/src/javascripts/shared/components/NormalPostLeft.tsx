9deaf2addd1eb8916c2d198e70c5ef91d6ea4009
Masaya Takahashi <masaya.oor710@gmail.com>
2018-09-21 23:40:57 +0900

    Create 'NormalPostLeft'

---
+++ frontend/src/javascripts/shared/components/NormalPostLeft.tsx
@@ -0,0 +1,23 @@
+import * as React from 'react';
+
+interface Props {
+  mysize_id: string;
+  image_url: string;
+  size: number;
+}
+
+const NormalPostLeft = (props: Props) => {
+  const { mysize_id, image_url, size } = props;
+  return (
+    <div className="index-image kpost-icon abs-center">
+      <a className="list-image" href={`/${mysize_id}?display=square`}>
+        <img src={user.image_url} alt={mysize_id} width="40" height="40" />
+      </a>
+      <div className="index-size kpost-shoesize">
+        <span>{size}</span>
+      </div>
+    </div>
+  );
+};
+
+export { NormalPostLeft };
