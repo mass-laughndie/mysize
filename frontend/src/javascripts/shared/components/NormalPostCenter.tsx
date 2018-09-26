cd02f7a81577670b1bf1aecb56f0c017e0b7ddcf
masa-laughndie <masaya.oor710@gmail.com>
2018-09-27 01:21:30 +0900

    Add NormalPostCenter

---
+++ frontend/src/javascripts/shared/components/NormalPostCenter.tsx
@@ -0,0 +1,35 @@
+import * as React from 'react';
+
+interface Props {
+  name: string;
+  mysize_id: string;
+  brand: string;
+  color: string;
+  title: string;
+  content: string;
+}
+const NormalPostCenter = (props: Props) => {
+  const { name, mysize_id, brand, color, title, content } = props;
+  return (
+    <div className="kpost-center kickspost-center">
+      <div className="kpost-name over-name">
+        <a href={`/${mysize_id}`}>
+          {name}
+          <span>{`@${mysize_id}`}</span>
+        </a>
+      </div>
+      <div className="kpost-brand">
+        <span>{brand}</span>
+      </div>
+      <div className="kpost-title">
+        <h2>
+          {`${title} `}
+          <span>{`( ${color} )`}</span>
+        </h2>
+      </div>
+      <div className="kpost-content autolink over-text">
+        <span>{content}</span>
+      </div>
+    </div>
+  );
+};
