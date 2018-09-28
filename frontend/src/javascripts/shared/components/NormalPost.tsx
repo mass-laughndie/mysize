47773b37fed0751418a21d1232a9272c12778ea8
masa-laughndie <masaya.oor710@gmail.com>
2018-09-29 00:25:01 +0900

    Apply NormalPostAct

--- frontend/src/javascripts/shared/components/NormalPost.tsx
+++ frontend/src/javascripts/shared/components/NormalPost.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import { Post, User } from '../../types/commonTypes';
 import { NormalPostLeft } from './NormalPostLeft';
 import { NormalPostCenter } from './NormalPostCenter';
+import { NormalPostAct } from './NormalPostAct';
 
 interface Props {
   post: Post;
@@ -33,62 +34,11 @@ const NormalPost = (props: Props) => {
               title={post.title}
               content={post.content}
             />
-            <div className="kpost-act  kickspost-act clear">
-              <div className="kpost-time">
-                {/* <%= time_ago_in_words(post.created_at) %>前 */}
-              </div>
-              <div className="kpost-alist">
-                <div className="kpost-aicon">
-                  {/* <%= render partial: 'goods/good_form',
-                            locals: { post: post,
-                                      type: type } %> */}
-                </div>
-                <div className="kpost-num" id={`good-num-kickspost-${post.id}`}>
-                  <a href={`/kicksposts/${post.id}/gooders`}>{'0'}</a>
-                  {/* <%= link_to post.goods.size, gooders_kickspost_path(post) %> */}
-                </div>
-              </div>
-              <div className="kpost-alist">
-                <a
-                  className="kpost-alink"
-                  href={`${user.mysize_id}/kicksposts/${post.id}`}
-                >
-                  <div className="kpost-aicon">
-                    <i className="fa fa-comment-o" />
-                  </div>
-                  <div className="kpost-num">
-                    <span>
-                      {'0'}
-                      {/* {post.comments.size} */}
-                    </span>
-                  </div>
-                </a>
-              </div>
-              <div className="kpost-alist alist-last">
-                <a
-                  className="kpost-alink"
-                  href={`https://twitter.com/intent/tweet?text=${encodedURI}&url=${
-                    window.location.origin
-                  }/${user.mysize_id}/kicksposts/${post.id}`}
-                  // onclick="window.open(encodeURI(decodeURI(this.href)), 'tweetwindow', 'width=650, height=470, personalbar=0, toolbar=0, scrollbars=1, sizable=31'); return false;"
-                >
-                  <div className="kpost-aicon">
-                    <i className="fa fa-twitter" />
-                  </div>
-                </a>
-                {/* <%= link_to "https://twitter.com/intent/tweet?text=" +
-                    ERB::Util.url_encode(post.user.name + "さんの投稿｜" +
-                                          post.title + "\n") +
-                    "&url=" + kickspost_url(post.user, post),
-                    className: "kpost-alink",
-                    onclick: "window.open(encodeURI(decodeURI(this.href)), 'tweetwindow', 'width=650, height=470, personalbar=0, toolbar=0, scrollbars=1, sizable=1'); return false;" do %>
-                  <div className="kpost-aicon">
-                    <i className='fa fa-twitter' />
-                    <%= fa_icon('twitter') %>
-                  </div>
-                <% end %> */}
-              </div>
-            </div>
+            <NormalPostAct
+              post_id={post.id}
+              mysize_id={user.mysize_id}
+              encodedURI={encodedURI}
+            />
             <div className="kpost-right kickspost-right">
               <div className="c kpost-right-top">
                 <div className="kpost-menu  kickspost-menu clear">
