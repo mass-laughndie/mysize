90e070c3b655b1d7631b8f65d1c3393f97d68e18
masa-laughndie <masaya.oor710@gmail.com>
2018-10-08 00:25:09 +0900

    Add classes for rails local

--- frontend/src/javascripts/shared/components/SquarePosts/SquarePost.tsx
+++ frontend/src/javascripts/shared/components/SquarePosts/SquarePost.tsx
@@ -11,18 +11,23 @@ const SquarePost = (props: Props) => {
   const { id, title, size, picture_url } = props.post;
   const { mysize_id } = props.post.postUser;
   return (
-    <li id={`square-${id}`} className={styles['square-list']}>
-      <div className={styles['square-picture']}>
+    <li
+      id={`square-${id}`}
+      className={classnames(styles['square-list'], 'square-list')}
+    >
+      <div className={classnames(styles['square-picture'], 'square-picture')}>
         <a href={`/${mysize_id}/kicksposts/${id}`}>
           <img
-            className={classnames(styles.cover, 'lazyload')}
+            className={classnames(styles.cover, 'cover', 'lazyload')}
             src="/images/grey.gif"
             data-src={picture_url}
             alt={title}
           />
         </a>
       </div>
-      <div className={styles['square-size']}>{size.toFixed(1)}</div>
+      <div className={classnames(styles['square-size'], 'square-size')}>
+        {size.toFixed(1)}
+      </div>
     </li>
   );
 };
