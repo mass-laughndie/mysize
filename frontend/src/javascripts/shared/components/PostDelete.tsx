268c83b1302b7d217d06199fdc1f35c31a388395
masa-laughndie <masaya.oor710@gmail.com>
2018-10-07 12:53:19 +0900

    Create PostDelete

---
+++ frontend/src/javascripts/shared/components/PostDelete.tsx
@@ -0,0 +1,33 @@
+import * as React from 'react';
+import { Post } from '../../types/commonTypes';
+
+interface Props {
+  post: Post;
+}
+
+const PostDelete = (props: Props) => {
+  const postDeleteUrl = (post: Post) => {
+    const rootUrl = location.protocol + '//' + location.host;
+    const { id, postType, postUser } = post;
+
+    if (postType == 'kickspost') {
+      return `${rootUrl}/${postUser.mysize_id}/kickspost/${id}`;
+    } else {
+      return `${rootUrl}/postcomment/${id}`;
+    }
+  };
+
+  return (
+    <a
+      data-confirm="本当に削除してよろしいですか？"
+      rel="nofollow"
+      data-method="delete"
+      href={postDeleteUrl(props.post)}
+    >
+      <i className="fa fa-trash" />
+      {' 投稿を削除する'}
+    </a>
+  );
+};
+
+export { PostDelete };
