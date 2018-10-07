11b61d7b6ac52225af48517eebf34f6c752ef5df
masa-laughndie <masaya.oor710@gmail.com>
2018-10-07 18:46:50 +0900

    Apply css module to SquarePost

--- frontend/src/javascripts/shared/components/SquarePosts/SquarePost.tsx
+++ frontend/src/javascripts/shared/components/SquarePosts/SquarePost.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import { Post } from '../../../types/commonTypes';
+import * as styles from './SquarePost.module.scss';
 
 interface Props {
   post: Post;
@@ -9,18 +10,18 @@ const SquarePost = (props: Props) => {
   const { id, title, size, picture_url } = props.post;
   const { mysize_id } = props.post.postUser;
   return (
-    <li id={`square-${id}`} className="square-list">
-      <div className="square-picture">
+    <li id={`square-${id}`} className={styles['square-list']}>
+      <div className={styles['square-picture']}>
         <a href={`/${mysize_id}/kicksposts/${id}`}>
           <img
-            className="cover lazyload"
+            className={`${styles.cover} lazyload`}
             src="/images/grey.gif"
             data-src={picture_url}
             alt={title}
           />
         </a>
       </div>
-      <div className="square-size">{size.toFixed(1)}</div>
+      <div className={styles['square-size']}>{size.toFixed(1)}</div>
     </li>
   );
 };
