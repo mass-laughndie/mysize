df262a467230cf11a067e6fe05e31bda38289d3f
masa-laughndie <masaya.oor710@gmail.com>
2018-10-07 12:24:08 +0900

    Fix the story of SquarePost

--- frontend/src/javascripts/shared/components/SquarePost.story.tsx
+++ frontend/src/javascripts/shared/components/SquarePost.story.tsx
@@ -9,4 +9,30 @@ const posts = {
   picture_url: '/uploads/kickspost/picture/29/x0jUhnaA8g.jpg'
 };
 
-storiesOf('SquarePost', module).add('default', () => <SquarePost {...posts} />);
+const post = {
+  id: 2,
+  user_id: 1,
+  brand: 'Nike',
+  color: 'Black Emerald',
+  title: 'Air Jordan1',
+  content: 'カメレオン',
+  size: 27,
+  picture_url: '/uploads/kickspost/picture/2/NAW30EGDow.jpg',
+  postType: 'kickspost',
+  postUser: {
+    content: 'test1',
+    id: 1,
+    image_url: '',
+    mysize_id: 'test1',
+    name: 'test1',
+    size: 27
+  },
+  goodNum: 2,
+  commentNum: 4,
+  isGood: true,
+  isMyPost: false
+};
+
+storiesOf('SquarePost', module).add('default', () => (
+  <SquarePost post={post} />
+));
