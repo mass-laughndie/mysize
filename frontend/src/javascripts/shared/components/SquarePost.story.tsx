2670f4235499ac78e300ea69ff9357278e0a2667
masa-laughndie <masaya.oor710@gmail.com>
2018-10-07 12:30:19 +0900

    Delete no user props on the SquarePost story

--- frontend/src/javascripts/shared/components/SquarePost.story.tsx
+++ frontend/src/javascripts/shared/components/SquarePost.story.tsx
@@ -2,13 +2,6 @@ import * as React from 'react';
 import { storiesOf } from '@storybook/react';
 import { SquarePost } from './SquarePost';
 
-const posts = {
-  id: 29,
-  title: 'Air Max',
-  size: 28,
-  picture_url: '/uploads/kickspost/picture/29/x0jUhnaA8g.jpg'
-};
-
 const post = {
   id: 2,
   user_id: 1,
