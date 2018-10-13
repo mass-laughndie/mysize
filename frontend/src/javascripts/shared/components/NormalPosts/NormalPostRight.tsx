899101b2dff2553a333dac711375a840751dce87
masa-laughndie <masaya.oor710@gmail.com>
2018-10-13 19:43:27 +0900

    Apply css for NormalPostRight

--- frontend/src/javascripts/shared/components/NormalPosts/NormalPostRight.tsx
+++ frontend/src/javascripts/shared/components/NormalPosts/NormalPostRight.tsx
@@ -1,6 +1,8 @@
 import * as React from 'react';
 import { Post } from '../../../types/commonTypes';
 import { PostMenu } from '../PostMenu';
+import classnames from 'classnames';
+import * as styles from './NormalPost.module.scss';
 
 interface Props {
   post: Post;
@@ -13,23 +15,37 @@ const NormalPostRight = (props: Props) => {
   const { mysize_id } = props.post.postUser;
 
   return (
-    <div className={`kpost-right ${postType}-right`}>
-      <div className="c kpost-right-top">
-        <div className={`kpost-menu  ${postType}-menu clear`}>
+    <div
+      className={classnames(styles['kpost-right'], styles[`${postType}-right`])}
+    >
+      <div className={classnames(styles.c, styles['kpost-right-top'])}>
+        <div
+          className={classnames(
+            styles['kpost-menu'],
+            styles[`${postType}-menu`],
+            styles.clear
+          )}
+        >
           {isMyPost && (
             <PostMenu post={props.post} twitterShareUrl={twitterShareUrl} />
           )}
         </div>
-        <div className="kpost-picture abs-center">
+        <div
+          className={classnames(styles['kpost-picture'], styles['abs-center'])}
+        >
           <a href={`/${mysize_id}/kicksposts/${id}?display='picture'`}>
             <img
-              className="cover lazyload"
+              className={classnames(styles.cover, 'lazyload')}
               src="/images/grey.gif"
               data-src={picture_url}
               alt={title}
             />
           </a>
-          <div className="kpost-size index-size">{size}</div>
+          <div
+            className={classnames(styles['kpost-size'], styles['index-size'])}
+          >
+            {size}
+          </div>
         </div>
       </div>
     </div>
