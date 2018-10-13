21d4eaf8bda18b626369e0f42e422a7e0912afc4
masa-laughndie <masaya.oor710@gmail.com>
2018-10-13 19:38:30 +0900

    Apply css for NormalPostAct

--- frontend/src/javascripts/shared/components/NormalPosts/NormalPostAct.tsx
+++ frontend/src/javascripts/shared/components/NormalPosts/NormalPostAct.tsx
@@ -1,6 +1,8 @@
 import * as React from 'react';
 import GoodForm from '../GoodForm/GoodForm';
 import { Post } from '../../../types/commonTypes';
+import classnames from 'classnames';
+import * as styles from './NormalPost.module.scss';
 
 interface Props {
   post: Post;
@@ -14,34 +16,43 @@ const NormalPostAct = (props: Props) => {
   const { twitterShareUrl, logged_in } = props;
 
   return (
-    <div className={`kpost-act  ${postType}-act clear`}>
-      <div className="kpost-alist">
-        <div className="kpost-aicon">
+    <div
+      className={classnames(
+        styles['kpost-act'],
+        styles[`${postType}-act`],
+        styles.clear
+      )}
+    >
+      <div className={styles['kpost-alist']}>
+        <div className={styles['kpost-aicon']}>
           <GoodForm logged_in={logged_in} post={props.post} />
         </div>
-        <div className="kpost-num" id={`good-num-${postType}-${id}`}>
+        <div className={styles['kpost-num']} id={`good-num-${postType}-${id}`}>
           <a href={`/kicksposts/${id}/gooders`}>{goodNum}</a>
         </div>
       </div>
-      <div className="kpost-alist">
-        <a className="kpost-alink" href={`${mysize_id}/kicksposts/${id}`}>
-          <div className="kpost-aicon">
+      <div className={styles['kpost-alist']}>
+        <a
+          className={styles['kpost-alink']}
+          href={`${mysize_id}/kicksposts/${id}`}
+        >
+          <div className={styles['kpost-aicon']}>
             <i className="fa fa-comment-o" />
           </div>
-          <div className="kpost-num">
+          <div className={styles['kpost-num']}>
             <span>{commentNum}</span>
           </div>
         </a>
       </div>
-      <div className="kpost-alist alist-last">
+      <div className={classnames(styles['kpost-alist'], styles['alist-last'])}>
         <a
-          className="kpost-alink"
+          className={styles['kpost-alink']}
           href={twitterShareUrl}
           onClick={() =>
             "window.open(encodeURI(decodeURI(this.href)), 'tweetwindow', 'width=650, height=470, personalbar=0, toolbar=0, scrollbars=1, sizable=31'); return false;"
           }
         >
-          <div className="kpost-aicon">
+          <div className={styles['kpost-aicon']}>
             <i className="fa fa-twitter" />
           </div>
         </a>
