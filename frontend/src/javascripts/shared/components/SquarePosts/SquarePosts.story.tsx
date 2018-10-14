72a3956a4f3844b6234f9244a54a4cfb299b64bd
masa-laughndie <masaya.oor710@gmail.com>
2018-10-14 22:53:51 +0900

    Create SquarePosts story

---
+++ frontend/src/javascripts/shared/components/SquarePosts/SquarePosts.story.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import { storiesOf } from '@storybook/react';
+import { withKnobs } from '@storybook/addon-knobs';
+import { SquarePosts } from './SquarePosts';
+import { posts, baseStyle } from '../../../../../stories/dummyData';
+
+storiesOf('SquarePosts', module)
+  .addDecorator(withKnobs)
+  .add('default', () => (
+    <ul style={baseStyle}>
+      <SquarePosts posts={posts} />
+    </ul>
+  ));
