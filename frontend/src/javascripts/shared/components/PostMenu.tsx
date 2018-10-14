8733a1c1198e1446242d3cad9462a53724207c3e
masa-laughndie <masaya.oor710@gmail.com>
2018-10-14 14:14:43 +0900

    Apply css for PostMenu

--- frontend/src/javascripts/shared/components/PostMenu.tsx
+++ frontend/src/javascripts/shared/components/PostMenu.tsx
@@ -1,54 +1,62 @@
 import * as React from 'react';
 import { Post } from '../../types/commonTypes';
 import { PostDelete } from './PostDelete';
+import classnames from 'classnames';
+import * as styles from './PostMenu.module.scss';
+import $ from 'jquery';
 
 interface Props {
   post: Post;
   twitterShareUrl: string;
 }
 
-const PostMenu = (props: Props) => {
-  const { id, postType, postUser } = props.post;
-  const { twitterShareUrl } = props;
-  const isKickspost = postType == 'kickspost';
-  return (
-    <React.Fragment>
-      <div id={`post-nav-${postType}-${id}`} className="post-nav">
-        <i className="fa fa-angle-down" />
-      </div>
-      <ul id={`nav-list-${postType}-${id}`} className="nav-list">
-        <li>
-          <PostDelete post={props.post} />
-        </li>
-        {isKickspost && (
-          <React.Fragment>
-            <li className="bar" />
-            <li>
-              <a href={`/${postUser.mysize_id}/kicksposts/${id}/edit`}>
-                <i className="fa fa-pencil-square-o" />
-                {' 投稿を編集する'}
-              </a>
-            </li>
-            <li className="bar" />
-            <li>
-              <a
-                className="kpost-alink"
-                href={twitterShareUrl}
-                onClick={() =>
-                  "window.open(encodeURI(decodeURI(this.href)), 'tweetwindow', 'width=650, height=470, personalbar=0, toolbar=0, scrollbars=1, sizable=31'); return false;"
-                }
-              >
-                <div className="kpost-aicon">
-                  <i className="fa fa-twitter" />
-                  {' Twitterでシェア'}
-                </div>
-              </a>
-            </li>
-          </React.Fragment>
-        )}
-      </ul>
-    </React.Fragment>
-  );
-};
+class PostMenu extends React.Component<Props> {
+  private;
+
+  public render() {
+    const { id, postType, postUser } = this.props.post;
+    const { twitterShareUrl } = this.props;
+    const isKickspost = postType == 'kickspost';
+
+    return (
+      <React.Fragment>
+        <div id={`post-nav-${postType}-${id}`} className={styles['post-nav']}>
+          <i className="fa fa-angle-down" />
+        </div>
+        <ul id={`nav-list-${postType}-${id}`} className={styles['nav-list']}>
+          <li>
+            <PostDelete post={this.props.post} />
+          </li>
+          {isKickspost && (
+            <React.Fragment>
+              <li className={styles['bar']} />
+              <li>
+                <a href={`/${postUser.mysize_id}/kicksposts/${id}/edit`}>
+                  <i className="fa fa-pencil-square-o" />
+                  {' 投稿を編集する'}
+                </a>
+              </li>
+              <li className="bar" />
+              <li>
+                <a
+                  className={styles['kpost-alink']}
+                  href={twitterShareUrl}
+                  onClick={() =>
+                    "window.open(encodeURI(decodeURI(this.href)), 'tweetwindow', 'width=650, height=470, personalbar=0, toolbar=0, scrollbars=1, sizable=31'); return false;"
+                  }
+                >
+                  <div className={styles['kpost-aicon']}>
+                    <i className="fa fa-twitter" />
+                    {' Twitterでシェア'}
+                  </div>
+                </a>
+              </li>
+            </React.Fragment>
+          )}
+        </ul>
+      </React.Fragment>
+    );
+  }
+}
 
 export { PostMenu };
