857c930f0c86edd031439dcf375b00ac3fc5682e
masa-laughndie <masaya.oor710@gmail.com>
2018-10-20 19:09:32 +0900

    Create some methods relating gon for Comments

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -5,6 +5,7 @@ class SearchesController < ApplicationController
     @kicksposts = Kickspost.search(params[:keyword]).includes(:user, {comments: :user})
     @comments = Comment.search(params[:keyword]).includes(:user)
     gon.searchKicksposts = Kickspost.find_format_gon_params(@kicksposts.ids.uniq, logged_in? ? current_user : nil) if params[:for] == "post"
+    gon.searchComments = Comment.find_format_gon_params(@comments.ids.uniq, logged_in? ? current_user : nil) if params[:for] == "com"
     gon.logged_in = logged_in?
   end
   
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -27,6 +27,46 @@ class Comment < ApplicationRecord
                            length:   { maximum: 500,
                                        message: "内容は500文字まで入力できます" }
 
+  class << self
+    def all_format_gon_params_for(user)
+      self.all.map do |post|
+        map_gon_hah(post, user)
+      end
+    end
+
+    def find_format_gon_params(ids, user)
+      self.find(ids).map do |post|
+        map_gon_hah(post, user)
+      end
+    end
+
+    def find_format_gon_params_by(id, user)
+      map_gon_hah(self.find_by(id), user)
+    end
+
+    private
+
+    def extract_params_for_gon
+      [:id, :user_id, :kickspost_id, :reply_id, :content]
+    end
+
+    def map_gon_hah(post, user)
+      return if post.blank?
+      post_type = post.class.name.downcase
+      Hash[
+        extract_params_for_gon.map do |ep|
+          [ep, post.send(ep)]
+        end
+      ].merge({
+        postType: post_type,
+        postUser: User.find_format_gon_params_by(post.user_id),
+        goodNum: post.goods.size,
+        isGood: (user.present? ? user.good?(post_type, post) : false),
+        isMyPost: user == post.user
+      })
+    end
+  end
+
   def good_notice_create_or_update
     return create_notice(user_id: self.user_id) if self.notice.nil?
     notice.add_unread_count!
