45812f7996ae9bd5c67525746702aefaeae57079
masa-laughndie <masaya.oor710@gmail.com>
2018-10-27 16:17:34 +0900

    Apply cc for UserListContent

--- frontend/src/javascripts/shared/components/UserLists/UserListContent.tsx
+++ frontend/src/javascripts/shared/components/UserLists/UserListContent.tsx
@@ -1,5 +1,7 @@
 import * as React from 'react';
+import classnames from 'classnames';
 import { User } from '../../../types/commonTypes';
+import * as styles from './UserListContent.module.scss';
 
 interface Props {
   user: User;
@@ -9,13 +11,22 @@ const UserListContent = (props: Props) => {
   const { name, mysize_id, content } = props.user;
 
   return (
-    <div className="index-right">
-      <div className="index-upper clear">
-        <div className="index-text">
-          <div className="index-name over-name">{name}</div>
-          <div className="index-msid key">{`@${mysize_id}`}</div>
+    <div className={styles['user-list-content']}>
+      <div className={classnames(styles['user-list-upper'], styles.clear)}>
+        <div className={styles['user-list-text']}>
+          <div
+            className={classnames(
+              styles['user-list-name'],
+              styles['over-name']
+            )}
+          >
+            {name}
+          </div>
+          <div
+            className={classnames(styles['user-list-msid'], 'key')}
+          >{`@${mysize_id}`}</div>
         </div>
-        <div className="index-follow">
+        <div className="user-list-follow">
           <div className="abs-center">
             {/* <% unless current_user?(user) %>
                   <%= render partial: 'users/follow_form',
