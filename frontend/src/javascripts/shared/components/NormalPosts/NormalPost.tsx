ffadd80d09c7dace37b1c6564f98e9ad94b4b925
masa-laughndie <masaya.oor710@gmail.com>
2018-10-27 15:42:00 +0900

    Apply List.module.scsss for NormalPost

--- frontend/src/javascripts/shared/components/NormalPosts/NormalPost.tsx
+++ frontend/src/javascripts/shared/components/NormalPosts/NormalPost.tsx
@@ -6,7 +6,7 @@ import { NormalPostLeft } from './NormalPostLeft';
 import { NormalPostCenter } from './NormalPostCenter';
 import { NormalPostAct } from './NormalPostAct';
 import { NormalPostRight } from './NormalPostRight';
-import * as styles from './NormalPost.module.scss';
+import * as styles from '../List.module.scss';
 
 interface Props {
   post: Post;
