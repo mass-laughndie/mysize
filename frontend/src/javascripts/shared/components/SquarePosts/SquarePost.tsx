9b0328e0cf3dbb269dbc7232e65d960be4adca09
masa-laughndie <masaya.oor710@gmail.com>
2018-11-10 18:49:38 +0900

    Fix the way applying css for on the /SquarePosts

--- frontend/src/javascripts/shared/components/SquarePosts/SquarePost.tsx
+++ frontend/src/javascripts/shared/components/SquarePosts/SquarePost.tsx
@@ -8,21 +8,22 @@ interface Props {
 }
 
 const SquarePost = (props: Props) => {
+  const cx = classnames.bind(styles);
   const { id, title, size, picture_url } = props.post;
   const { mysize_id } = props.post.postUser;
   return (
-    <li id={`square-${id}`} className={classnames(styles['square-list'])}>
-      <div className={classnames(styles['square-picture'])}>
+    <li id={`square-${id}`} className={cx('square-list')}>
+      <div className={cx('square-picture')}>
         <a href={`/${mysize_id}/kicksposts/${id}`}>
           <img
-            className={classnames(styles.cover, 'lazyload')}
+            className={cx(styles.cover, 'lazyload')}
             src="/images/grey.gif"
             data-src={picture_url}
             alt={title}
           />
         </a>
       </div>
-      <div className={classnames(styles['square-size'])}>
+      <div className={cx('square-size')}>
         {typeof size === 'number' && size.toFixed(1)}
       </div>
     </li>
