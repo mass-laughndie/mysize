71eedd77c2da44920d57fceaaef1f290785b6a37
masa-laughndie <masaya.oor710@gmail.com>
2018-12-02 19:25:26 +0900

    Move FollowApi

--- frontend/src/javascripts/modules/FollowForm/apis/FollowApi.ts
+++ frontend/src/javascripts/modules/FollowForm/apis/FollowApi.ts
@@ -1,4 +1,4 @@
-import axios from '../../modules/Shared/apis/common/axios';
+import axios from '../../Shared/apis/common/axios';
 
 export const followUser = async (id: number): Promise<any> => {
   const response = await axios
