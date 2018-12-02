25af024a78362f35546ea58aa3dfac09a18f05de
masa-laughndie <masaya.oor710@gmail.com>
2018-12-02 16:21:01 +0900

    Move FollowForm

--- frontend/src/javascripts/modules/NormalPosts/components/NormalPostAct.tsx
+++ frontend/src/javascripts/modules/NormalPosts/components/NormalPostAct.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import GoodForm from '../../../shared/components/GoodForm';
+import GoodForm from '../../FollowForm/components';
 import { Post, CurrentInfo } from '../../../types/commonTypes';
 import classnames from 'classnames';
 import * as styles from './NormalPost.module.scss';
