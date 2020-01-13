e8c1f4cd15fa4c2ed78eeb4aebeda1d8cc64ccaf
masa-laughndie <masaya.oor710@gmail.com>
2020-01-13 16:19:36 +0900

    remove no use code

--- frontend/src/javascripts/pages/test/index.tsx
+++ frontend/src/javascripts/pages/test/index.tsx
@@ -3,21 +3,3 @@ import * as ReactDOM from 'react-dom';
 import Hello from '../../features/Hello/components';
 
 ReactDOM.render(<Hello />, document.querySelector('[data-react-entry="root"]'));
-
-function switchPlanTable() {
-  const tabIdNamePrefix = 'js-plan-table-tab';
-  const activeTabNum = Number(
-    $(`[id^=${tabIdNamePrefix}]`)
-      .filter("[class$='is-active']")
-      .attr('id')
-      .replace(tabIdNamePrefix, '')
-  );
-
-  if (activeTabNum > 0) {
-    const tableIdNamePrefix = 'js-plan-table';
-    $(`[id^=${tableIdNamePrefix}]`).hide();
-    $(`${tableIdNamePrefix}${activeTabNum}`).show();
-  }
-}
-
-switchPlanTable();
